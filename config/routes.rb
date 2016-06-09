dfd1a509e442239613b16525851add65c4b7166e
Morgunov <morgunov.igor1990@gmail.com>
2016-06-09 18:01:01 +0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  resources :products
+ get "store/index"
+resources :products
+# The priority is based upon order of creation:
+# (Приоритет основан на порядке создания:)
+# first created -> highest priority.
+# (создан первым -> наивысший приоритет.)
+# See how all your routes lay out with "rake routes".
+# (Раскладку всех маршрутов можно увидеть с помощью команды "rake routes".)
+# You can have the root of your site routed with "root"
+# (Корневой маршрут к вашему сайту можно получить с помощью "root")
+root to: 'store#index', as: 'store'
+# ...
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
