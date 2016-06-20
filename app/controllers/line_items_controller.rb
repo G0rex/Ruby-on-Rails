a1cfd48c00090664c2ba332e7dfae3fc55f3fc6d
Morgunov <morgunov.igor1990@gmail.com>
2016-06-20 13:37:05 +0300

    additional tasks

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -37,6 +37,7 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
+    session[:counter]=0
     #binding.pry
     respond_to do |format|
       if @line_item.save
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -11,7 +11,13 @@ class StoreController < ApplicationController
   skip_before_action :authorize
   before_action :set_cart
   def index
+  	if session[:counter].nil?
+    		session[:counter]=0
+    	else 
+    		session[:counter]+=1
+    	end
     if params[:set_locale]
+    	
  redirect_to store_url(locale: params[:set_locale])
  else
  @products = Product.order(:title)
